9e42835ca41b06c7a6122a4eea44cce52c90671b
Fredy Rojas <frerojas@vonneumann.pe>
2020-11-11 11:35:44 -0500

    Modificando Migraciones de Ubigeo y Empresa

--- emp/models.py
+++ emp/models.py
@@ -2,12 +2,36 @@ from django.db import models
 from bases.models import ClaseModelo
 
 # Create your models here.
+#Modelo Departamento
+class Departamento(models.Model):
+    codigo= models.CharField(max_length=2,help_text="Código Departamento")
+    nombre= models.CharField(max_length=191,help_text='Descripción Departamento')
+
+    class Meta:
+        verbose_name_plural= 'Departamentos'
+
+class Provincia(models.Model):
+    codigo = models.CharField(max_length=4, help_text="Codigo Provincia")
+    nombre = models.CharField(max_length=191, help_text="Descripción de Provincia")
+    departamento = models.ForeignKey(Departamento,on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Provincias'
+
+class Distrito(models.Model):
+    codigo = models.CharField(max_length=6, help_text="Codigo Distrito")
+    nombre = models.CharField(max_length=191, help_text="Descripción de Distrito")
+    provincia = models.ForeignKey(Provincia,on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Distritos'   
+
 class Empresa(ClaseModelo):
     ruc = models.CharField(max_length=11,help_text=" R.U.C. de Empresa",unique=True)
     razon_social = models.CharField(max_length=191,help_text="Razón Social Empresa")
     nombre_comercial = models.CharField(max_length=191, help_text="Nombre Comercial Empresa",blank=True, null=True)
     direccion_principal = models.CharField(max_length=191,help_text="Dirección Empresa")
-    ubigeo = models.CharField(max_length=191,help_text="Ubigeo Empresa")
+    ubigeo = models.ForeignKey(Distrito,on_delete=models.CASCADE,blank=True, null=True)
     pagina_web = models.CharField(max_length=191,blank=True, null=True,help_text="URL / Dirección Web")
     usuario_sol = models.CharField(max_length=50,blank=True, null=True,help_text="Usuario Clave Sol")
     clave_sol = models.CharField(max_length=50, blank=True, null=True, help_text="COntraseña Clave Sol")
@@ -29,3 +53,8 @@ class Empresa(ClaseModelo):
 
     class Meta:
         verbose_name_plural= 'Empresas' 
+
+# class Sucursal(ClaseModelo):
+#     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+#     nombre = models.CharField(max_length=191)
+#     direccion = models.CharField(max_length=191)
